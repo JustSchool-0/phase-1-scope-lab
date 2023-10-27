19ced54142ee4a47ce8344e9730863e5e4042f6b
Me <pdprince.lfc+github@gmail.com>
2023-10-27 14:27:58 -0400

    many error hehe

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// noinspection ES6ConvertVarToLetConst
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  // noinspection JSUndeclaredVariable
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  // noinspection JSUndeclaredVariable
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'ted';
+
+function changeLeastFavoriteCustomer() {
+  // noinspection JSConstantReassignment
+  leastFavoriteCustomer = 'bob';
+}
